2d0d305667db9cac8f4de48934b74bc9785714b6
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-02-16 08:46:41 +0100

    task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "tasks#list"
+  get "/task/new", to: "tasks#new", as: "new_task"
+  post "/task/save", to: "tasks#save", as: "save_task"
+  get "/task/:id", to: "tasks#show", as: "task"
 end
